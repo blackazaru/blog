27a0924b68d7fd23c1f8d9fff24ef44683d61c92
Danik <danik.cyrkunov@gmail.com>
2014-07-16 17:47:34 +0300

    add code

---
+++ blog.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"net/http"
+)
+
+func main() {
+	m := martini.Classic()
+	m.Use(render.Renderer(
+		render.Options{
+			Directory: "public",
+			Extensions: []string{".tmpl", ".html"},
+		},
+	))
+
+	m.Get("/", func(w http.ResponseWriter, rr *http.Request,r render.Render) {
+			http.Redirect(w, rr, "/home", http.StatusFound)
+			r.HTML(200, "home/index", nil)
+		})
+
+	m.Run()
+}
